af9bb51aafe6d3834640a454fbcd5943097e051c
DESKTOP-RT9K64F\Renz <lilfighter__@hotmail.com>
2018-06-13 16:25:56 -0600

    - Working copy. Demo'd copy. Demo'd to Dr.Kim Adams and Robert Morgan (UK guy)

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,22 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameControl : MonoBehaviour {
 
     public GameObject effectorTop;
     public GameObject effectorBottom;
+    public GameObject Ball;
     public float endEffectorLength;
 
     private Transform topTransform;
     private Transform bottomTransform;
     private float timeBefore = 0f;
     private float timeDiff;
+    private Vector3 ballPosition;
 
 	// Use this for initialization
 	void Start () {
         topTransform = effectorTop.GetComponent<Transform>();
         bottomTransform = effectorBottom.GetComponent<Transform>();
+        ballPosition = Ball.GetComponent<Transform>().position;
 	}
 
 
@@ -28,6 +32,11 @@ public class GameControl : MonoBehaviour {
         if (Input.GetKey(KeyCode.Q))
         {
             Debug.Log(endEffectorLength);
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            Ball.GetComponent<Transform>().position = ballPosition;
+            Ball.GetComponent<Rigidbody>().velocity = new Vector3(0,0,0);
         }
     }
 }
